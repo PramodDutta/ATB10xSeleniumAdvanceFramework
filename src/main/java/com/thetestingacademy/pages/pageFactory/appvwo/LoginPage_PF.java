48b93b7d2026502a970d6a7e29d8ca6534fbe9f4
Promode <pramoddutta_96@yahoo.com>
2025-04-13 09:53:05 +0530

    LOG4J, PAGE FACTORY

--- src/main/java/com/thetestingacademy/pages/pageFactory/appvwo/LoginPage_PF.java
+++ src/main/java/com/thetestingacademy/pages/pageFactory/appvwo/LoginPage_PF.java
@@ -1,4 +1,67 @@
 package com.thetestingacademy.pages.pageFactory.appvwo;
 
-public class LoginPage_PF {
+import com.thetestingacademy.base.CommonToAllPage;
+import com.thetestingacademy.utils.PropertiesReader;
+import com.thetestingacademy.utils.WaitHelpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.*;
+
+import java.util.List;
+
+public class LoginPage_PF extends CommonToAllPage {
+
+    WebDriver driver;
+    public LoginPage_PF(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(id="login-username")
+    private WebElement username;
+
+    @FindBy(name = "password")
+    private WebElement password;
+
+    @FindBy(id = "js-login-btn")
+    private WebElement signButton;
+
+    @FindBy(css = "#js-notification-box-msg")
+    private WebElement error_message;
+
+//    @FindBy(id = "js-login-btn")
+//    private List<WebElement> list_element;
+
+//    @FindBy(xpath = "js-login-btn")
+//    private WebElement signButton;
+
+//    @FindBy(css = "#js-notification-box-msg")
+//    private WebElement error_message;
+
+    //  Multiple Locator Types | OR condition (any criteria can match)
+//    @FindAll(
+//            {
+//                    @FindBy(how = How.ID, using = "login-username"),
+//                    @FindBy(name = "username")
+//            }
+//    )
+//    private WebElement input_element;
+
+
+
+    // WebElement username = driver.findElement(By.username("login-username");
+
+
+    // Page Actions
+    public String loginToVWOInvalidCreds() {
+        enterInput(username, PropertiesReader.readKey("invalid_username"));
+        enterInput(password, PropertiesReader.readKey("invalid_password"));
+        clickElement(signButton);
+        WaitHelpers.waitJVM(5000);
+        return getText(error_message);
+
+    }
+
+
 }
--- src/main/java/com/thetestingacademy/utils/WaitHelpers.java
+++ src/main/java/com/thetestingacademy/utils/WaitHelpers.java
@@ -72,6 +72,9 @@ public class WaitHelpers {
     public static WebElement visibilityOfElement(By elementLocation) {
         return new WebDriverWait(getDriver(), Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOfElementLocated(elementLocation));
     }
+    public static WebElement visibilityOfElement(WebElement elementLocation) {
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOf(elementLocation));
+    }
 
     public WebElement getElement(By key) {
         return getDriver().findElement(key);
